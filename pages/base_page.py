57c0f83846788e23e6b2e4a0204190b94f67e502
druni129 <druni129@mail.ru>
2021-11-09 11:46:41 +0300

    Реализация понятного вывода ошибки

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url,timeout=5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout) #Неявное ожидание
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        # перехватим исключение
+        except (NoSuchElementException):
+            return False
+        return True
